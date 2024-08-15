206b76f714ce074d0137dbd9d274f17be5c44551
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-08-15 11:57:25 +0000

    Fix native file upload + bump to v0.1.30

--- ipyfilite/upload/native.py
+++ ipyfilite/upload/native.py
@@ -5,6 +5,7 @@
 # Distributed under the terms of the Modified BSD License.
 
 from pathlib import Path
+from types import SimpleNamespace
 
 
 class FileUploadLite:
@@ -14,7 +15,7 @@ class FileUploadLite:
     async def request(self):
         path = input("Enter filepath:")
 
-        self.value = (dict(name=Path(path).name, path=Path(path)),)
+        self.value = (SimpleNamespace(name=Path(path).name, path=Path(path)),)
 
         return self.value
 
